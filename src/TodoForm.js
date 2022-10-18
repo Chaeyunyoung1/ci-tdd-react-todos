fe71166374fca454d33f37473b621cdef40165f4
Chaeyunyoung1 <sdfnkl@naver.com>
2022-10-18 09:40:15 +0900

    second commit

--- src/TodoForm.js
+++ src/TodoForm.js
@@ -1,22 +1,27 @@
-    import React, { useCallback, useState } from 'react';
+import React, { useCallback, useState } from 'react';
 
-    const TodoForm = ({onInsert}) => {
-        const [value, setValue] = useState('');
-        const onChange = useCallback(e => {
-            setValue(e.target.value);
-            // console.log(value);
-        }, []);
-        const onSubmit = useCallback(e => {
-            onInsert(value);
-            setValue('');
-            e.preventDefault(); // 새로고침 방지
-        }, [onInsert, value]);
-        return (<div>
+const TodoForm = ({ onInsert }) => {
+    const [value, setValue] = useState('');
+
+    const onChange = useCallback(e => {
+        setValue(e.target.value);
+        // console.log(value);
+    }, []);
+
+    const onSubmit = useCallback(e => {
+        onInsert(value);
+        setValue('');
+        e.preventDefault(); // 새로고침 방지
+    }, [onInsert, value]);
+    
+    return (
+        <div>
             <form onSubmit={onSubmit}>
-                <input value ={value} onChange={onChange} placeholder='할 일을 입력하세요' />
-                <button type='submit'>등록</button>
+                <input value={value} onChange={onChange} placeholder="할 일을 입력하세요" />
+                <button type="submit">등록</button>
             </form>
-        </div>);
-    };
+        </div>
+    );
+};
 
-    export default TodoForm;
+export default TodoForm;
